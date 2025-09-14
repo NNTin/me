1b38b9b6154c342d5072f27fecda1847ecfbe46d
Nguyen-Ngoc Ti <tnguyen@itq.de>
2025-09-14 18:45:51 +0200

    make sure Vercel is building into the subpath

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ set -e
 bundle install
 
 # Build the Jekyll site to the _site directory
-bundle exec jekyll build
+bundle exec jekyll build --destination _site/me
+
 # Add .nojekyll to prevent GitHub Pages from rebuilding the site
-touch _site/.nojekyll
+touch _site/me/.nojekyll
